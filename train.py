f8f8677f2421227599203f884f8a388f5c3198b1
Nithya <156448129+Nityaarv27@users.noreply.github.com>
2024-12-08 19:20:09 +0530

    added train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import json
 
-from sympy import Idx
+
 from nltk_utils import tokenize,stem,bag_of_words
 import numpy as np
 
@@ -28,14 +28,13 @@ ignore_words = ['?','!','.',',']
 all_words =[stem(w) for w in all_words if w not in ignore_words ]
 all_words=sorted(set(all_words))
 tags=sorted(set(tags))
-print(tags)
+
 
 X_train=[]
 Y_train=[]
 for (pattern_sentence,tag) in xy:
     bag = bag_of_words(pattern_sentence,all_words)
     X_train.append(bag)
-
     label =tags.index(tag)
     Y_train.append(label)
 X_train=np.array(X_train)
@@ -44,25 +43,56 @@ Y_train=np.array(Y_train)
 class ChatDataset(Dataset):
     def __init__(self):
       self.n_samples = len(X_train)
-      self.x_data = X_train
-      self.y_data= Y_train
+      self.x_data = torch.tensor(X_train, dtype=torch.float32)
+      self.y_data = torch.tensor(Y_train, dtype=torch.long)
+      
+      #self.x_data= X_train
+      #self.y_data= Y_train
 
     def __getitem__(self, index):
-        return self.x_data[Idx],self.y_data[Idx]
+        return self.x_data[index],self.y_data[index]
     def __len__(self):
         return self.n_samples
+    
+#Hyperparameters
 
 batch_size=8
 hidden_size = 8
 output_size = len(tags)
 input_size = len(X_train[0])
-print(input_size,len(all_words))
-print(output_size,tags)
+learning_rate=0.001
+num_epochs=1000
 
 
 
-    
+
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,batch_size=batch_size, shuffle=True,num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size= batch_size, shuffle=True,num_workers=0)
 
-model = NeuralNet(input_size, hidden_size,output_size)
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+models = NeuralNet(input_size, hidden_size,output_size).to(device)
+
+#loss and optimizer
+criterion =nn.CrossEntropyLoss()
+optimizer=torch.optim.Adam(models.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words,labels) in train_loader:
+        words = words.to(device)
+        labels= labels.to(device)
+
+        #forward
+        outputs = models(words)
+        loss = criterion(outputs,labels)
+
+        #backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch +1) % 100 == 0:
+        print(f'epoch {epoch +1}/{num_epochs}, loss={loss.item():.4f}')
+        
+print(f'final loss, loss={loss.item():.4f}')
+
+
